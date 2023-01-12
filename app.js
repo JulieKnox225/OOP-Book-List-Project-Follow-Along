caca346cf61c076b4ce69706cea812197d7ff509
Julie Knox <julie.knox.cohort.225@gmail.com>
2023-01-12 14:55:39 -0800

    add book to list and add validation and alert

---
+++ app.js
@@ -0,0 +1,92 @@
+//Book Constructor
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+//UI Constructor
+function UI() {}
+
+//add book to list
+UI.prototype.addBookToList = function(book) {
+    const list = document.getElementById('book-list');
+
+    //create tr element
+    const row = document.createElement('tr');
+
+    //insert cols
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X</a></td>
+    `;
+
+    list.appendChild(row);
+}
+
+//Show Alert
+UI.prototype.showAlert = function(message, className) {
+    //Create div
+    const div = document.createElement('div');
+
+    //Add Classes
+    div.className = `alert ${className}`;
+
+    //Add text
+    div.appendChild(document.createTextNode(message));
+
+    //Get parent
+    const container = document.querySelector('.container');
+
+    //Get form
+    const form = document.querySelector('#book-form');
+
+    //Insert alert
+    container.insertBefore(div, form);
+
+    //Timeout after three seconds
+    setTimeout(function() {
+        document.querySelector('.alert').remove();
+    }, 3000);
+}
+
+//Clear Fields
+UI.prototype.clearFields = function() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+}
+
+//Event Listeners
+document.getElementById('book-form').addEventListener('submit', 
+    function(e) {
+        //get form values
+        const title = document.getElementById('title').value,
+              author = document.getElementById('author').value,
+              isbn = document.getElementById('isbn').value;
+
+        //creating a book
+        const book = new Book(title, author, isbn);
+
+        //create ui
+        const ui = new UI();
+
+        //Validate
+        if(title === '' || author === '' || isbn === '') {
+            //error alert
+            ui.showAlert('Please fill in all fields', 'error');
+        } else {
+            //Add book to list
+            ui.addBookToList(book);
+
+            //Show alert
+            ui.showAlert('Book Added', 'success');
+
+            //clear fields
+            ui.clearFields();
+        }
+
+        e.preventDefault();
+});
